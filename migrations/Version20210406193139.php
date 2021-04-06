41fa16af04db060a57052cac7b78810f266ceeb8
unknown <erwan.bonniot@gmail.com>
2021-04-06 22:03:48 +0200

    Ajout de l'import et de l''ntite ITEM

---
+++ migrations/Version20210406193139.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210406193139 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE item ALTER color DROP NOT NULL');
+        $this->addSql('ALTER TABLE item ALTER profession DROP NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE item ALTER color SET NOT NULL');
+        $this->addSql('ALTER TABLE item ALTER profession SET NOT NULL');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,29 @@
 namespace App\Controller;
 
 
+use App\Entity\Item;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function __invoke(): Response
     {
-        return $this->render('home/index.html.twig');
+        $items = $this->em->getRepository(Item::class)->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'items' => $items,
+        ]);
     }
 }
