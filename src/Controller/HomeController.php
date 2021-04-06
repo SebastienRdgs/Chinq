2a546d9512d77d147a0629d2b818a6f1f5ab4011
unknown <erwan.bonniot@gmail.com>
2021-04-06 20:28:32 +0200

    Add webpack

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
-    public function __invoke()
+    public function __invoke(): Response
     {
         return $this->render('home/index.html.twig');
     }
