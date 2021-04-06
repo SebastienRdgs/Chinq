74c73370c749f3d26c3bf373b26860ba921880f8
Sébastien Rodrigues <sebastienb.rodrigues@gmail.com>
2021-04-06 21:13:18 +0200

    Ajout loader sass

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,10 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke()
     {
         return $this->render('home/index.html.twig');
     }
